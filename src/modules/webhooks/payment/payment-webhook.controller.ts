64d65f87d1ed0fd8f776896399a8bc6d55a35371
TruongNT03 <ntt26072003@gmail.com>
2025-10-30 15:23:40 +0700

    fix: webhook payment

--- src/modules/webhooks/payment/payment-webhook.controller.ts
+++ src/modules/webhooks/payment/payment-webhook.controller.ts
@@ -3,6 +3,7 @@ import { PaymentWebhookService } from './payment-webhook.service';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { SuccessResponseDto } from 'src/common/dto/success-response.dto';
 import { Webhook } from '@payos/node';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller('webhook/payment')
 export class PaymentWebhookController {
@@ -11,6 +12,7 @@ export class PaymentWebhookController {
   @ApiOperation({ summary: '[WEBHOOK] RECEIVED PAYMENT INFORMATION' })
   @ApiResponse({ status: 200, type: SuccessResponseDto })
   @Post()
+  @Public()
   async payment(@Body() body: Webhook): Promise<SuccessResponseDto> {
     return await this.paymentService.payment(body);
   }
